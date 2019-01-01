1d07bbf874c86d2fc1971358bce7627bf5a8b93d
Ranajoy Dutta <ranajoydutta7@gmail.com>
2019-01-01 21:16:10 +0530

    Solution in Python

--- One Dimensional Array/Find_Biggest_Difference.py
+++ One Dimensional Array/Find_Biggest_Difference.py
@@ -1,10 +1,9 @@
-"""
-Find Biggest Difference
+'''
+Find Biggest Difference 
 You will be given an array and you need to find the biggest difference between any two numbers present in the array.
 
 Input Format
 You will be taking a number as an input stdin which tells about the length of the array. On another line, array elements should be there with single space between them.
-
 Output Format
 You need to print the biggest difference between any two numbers present in the array.
 
@@ -14,18 +13,16 @@ Input
 29 79 72 81 9 7 21
 Output
 74
-"""
-
+'''
 def main():
-    arr = list(map(int, input().split()))
-    minn, maxx = arr[0], 0
+    input()
+    arr = list(map(int,input().split()))
+    minn,maxx = arr[0],arr[0]
     for i in arr:
         if i<minn:
             minn = i
-            continue
         elif i>maxx:
             maxx = i
+            
     print(maxx-minn)
-    
 main()
-
